f4cea43270e75c87061dd5fdd94be28119a9b31d
superavatar888 <superavatar888@gmail.com>
2025-10-09 16:39:24 +0800

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    "*.{js,ts,jsx,tsx,mdx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(214.3 31.8% 91.4%)",
+        input: "hsl(214.3 31.8% 91.4%)",
+        ring: "hsl(222.2 84% 4.9%)",
+        background: "hsl(0 0% 100%)",
+        foreground: "hsl(222.2 84% 4.9%)",
+        primary: {
+          DEFAULT: "hsl(222.2 47.4% 11.2%)",
+          foreground: "hsl(210 40% 98%)",
+        },
+      },
+      borderRadius: {
+        lg: "0.5rem",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
