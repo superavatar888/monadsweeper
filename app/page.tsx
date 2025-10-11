ab7556c4055abd91d3927bc923a65aa5d201f9d0
superavatar888 <superavatar888@gmail.com>
2025-10-11 17:37:51 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import MonadSweeperApp from "../components/MonadSweeperApp";
 
 export default function Home() {
   return (
-    // 🚀 FIX: 将背景更新为浅灰色，以匹配 Dashboard 风格
+    //  将背景更新为浅灰色，以匹配 Dashboard 风格
     <main className="min-h-screen bg-gray-100 font-sans">
       <MonadSweeperApp />
     </main>
