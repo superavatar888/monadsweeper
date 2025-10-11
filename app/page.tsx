ecf4f7fa90aa8c502ea468edf0be0a863482cef9
superavatar888 <superavatar888@gmail.com>
2025-10-11 18:24:44 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import MonadSweeperApp from "../components/MonadSweeperApp";
+import MonadSweeperApp from "../components/MonadSweeperApp"
 
 export default function Home() {
   return (
-    // 为主卡片提供一个浅灰色背景和垂直间距
-    <main className="min-h-screen bg-gray-100 font-sans py-12 px-4">
+    <main className="min-h-screen bg-gradient-to-br from-gray-100 to-blue-50 font-sans py-12 px-4">
       <MonadSweeperApp />
     </main>
-  );
+  )
 }
